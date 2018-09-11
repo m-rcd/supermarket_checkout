55685b8e0630101b8ddd99645f041a32056a65ca
Marianne Rachid <marianne.rachid@gmail.com>
2018-09-11 09:14:41 +0100

    add context

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,13 +1,17 @@
 require 'item'
 
 describe Item do
-  it 'it creates a new item with its price' do
-    item = Item.new('Bread',3)
-    expect(item).to have_attributes(name: 'bread', price: 3)
+  context 'testing #initialize method' do
+    it 'it creates a new item with its price' do
+      item = Item.new('Bread',3)
+      expect(item).to have_attributes(name: 'bread', price: 3)
+    end
   end
 
-  it 'shows price of item' do
-    item = Item.new('Bread',3)
-    expect(item.show_price).to eq(3)
+  context 'testing #show method' do
+    it 'shows price of item' do
+      item = Item.new('Bread',3)
+      expect(item.show_price).to eq(3)
+    end
   end
 end

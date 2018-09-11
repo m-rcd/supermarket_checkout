80047d45247fd228c86b4ccf6e0e147e61ddde87
Marianne Rachid <marianne.rachid@gmail.com>
2018-09-11 09:15:18 +0100

    add total of scanned items

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,8 +1,10 @@
 class Checkout
-  attr_reader :scanned_items
+  TOTAL = 0
+  attr_reader :scanned_items, :total
 
-  def initialize
+  def initialize(total = TOTAL)
     @scanned_items = {}
+    @total = total
   end
 
   def scan(item)
@@ -11,5 +13,10 @@ class Checkout
     else
       @scanned_items.store(item.name, item.price)
     end
+  end
+
+  def total
+    @scanned_items.map { |k, v| @total += v }
+    @total
   end
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -4,12 +4,15 @@ describe Checkout do
   let (:item1){double :item,:name => 'milk', :price => 1}
   let (:item2){double :item,:name => 'bread', :price => 2}
   let (:item3){double :item,:name => 'milk', :price => 1}
-  it 'scans items' do
-    checkout = Checkout.new
-    checkout.scan(item1)
-    checkout.scan(item2)
-    expect(checkout.scanned_items).to include('bread' => 2)
-    expect(checkout.scanned_items).to include('milk' => 1)
+
+  context 'testing #scan method' do
+    it 'scans items' do
+      checkout = Checkout.new
+      checkout.scan(item1)
+      checkout.scan(item2)
+      expect(checkout.scanned_items).to include('bread' => 2)
+      expect(checkout.scanned_items).to include('milk' => 1)
+    end
   end
 
   it 'adds price of similar items' do
@@ -18,5 +21,13 @@ describe Checkout do
     checkout.scan(item2)
     checkout.scan(item3)
     expect(checkout.scanned_items).to include('milk' => 2, 'bread' => 2)
+  end
+
+  it 'gets total for all scanned items' do
+    checkout = Checkout.new
+    checkout.scan(item1)
+    checkout.scan(item2)
+    checkout.scan(item3)
+    expect(checkout.total).to eq(4)
   end
 end
